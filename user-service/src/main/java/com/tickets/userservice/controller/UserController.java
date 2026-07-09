4b362b24c943702bc982fdbba187e520601e022a
Uana <oanavitalariu@yahoo.com>
2026-07-09 23:37:20 +0300

    restructure

---
+++ user-service/src/main/java/com/tickets/userservice/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.tickets.userservice.controller;
+
+
+import com.tickets.userservice.model.User;
+import com.tickets.userservice.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    public final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService =  userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user){
+        User savedUser = userService.register(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> credentials){
+        String username = credentials.get("username");
+        String password = credentials.get("password");
+
+        Optional<User> userOptional = userService.login(username,password);
+
+        if(userOptional.isPresent()){
+            return ResponseEntity.ok(userOptional.get());
+        } else {
+            Map<String, String> error = new HashMap<>();
+            error.put("message", "Invalid username or password");
+            return ResponseEntity.status(401).body(error);
+        }
+    }
+}
---
+++ user-service/src/main/java/com/tickets/userservice/service/UserService.java
@@ -0,0 +1,38 @@
+package com.tickets.userservice.service;
+
+import com.tickets.userservice.model.User;
+import com.tickets.userservice.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User register(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER");
+        return userRepository.save(user);
+    }
+
+    public Optional<User> login(String username, String rawPassword){
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if(userOptional.isPresent()){
+            User user = userOptional.get();
+            if(passwordEncoder.matches(rawPassword, user.getPassword())){
+                return Optional.of(user);
+            }
+        }
+    return Optional.empty();
+    }
+}
+
